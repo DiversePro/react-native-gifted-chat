0962fc5f1293764cb1b533d69c1373aa0cb4737e
champuu123 <71210857+champuu123@users.noreply.github.com>
2022-11-14 15:36:02 +0500

    Chat UI Points Done

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -3,5 +3,6 @@ import { IMessage } from './Models';
 export declare const StylePropType: PropTypes.Requireable<number | boolean | object>;
 export declare function isSameDay(currentMessage: IMessage, diffMessage: IMessage | null | undefined): boolean;
 export declare function isSameUser(currentMessage: IMessage, diffMessage: IMessage | null | undefined): boolean;
+export declare const isUnreadStarted: (currentMessage: IMessage, diffMessage: IMessage | null | undefined,user:any) => boolean;
 export declare const warning: (...args: any) => void;
 export declare const error: (...args: any) => void;
